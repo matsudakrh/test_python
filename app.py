2fa5fe30bd4545451cda41b3f2ac032224e6855a
matsudakrh <matsuda.ddcm@gmail.com>
2018-05-10 00:50:57 +0900

    sutras

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-def hoge():
-    print(f'hoge')
+from flask import Flask, render_template, request, redirect, url_for
+import numpy as np
 
-hoge()
+ap = Flask(__name__)
+
+
+def picked_up():
+    messages = [
+        "こんにちは、あなたの名前を入力してください",
+        "やあ！お名前は何ですか？",
+        "あなたの名前を教えてね",
+    ]
+
+    return np.random.choice(messages)
+
+
+txt = picked_up()
+print(txt)
